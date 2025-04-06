d7288922ccb582fe2ec4332d503616f595efb274
Pedro Cabrera <pedrocabrera@Pedros-MacBook-Pro.local>
2025-04-06 11:44:08 -0400

    Primer commit en con sección de videos

--- src/components/ui/button.jsx
+++
@@ -1,8 +0,0 @@
-
-export function Button({ children, className }) {
-  return (
-    <button className={`text-white font-semibold py-2 px-4 rounded-xl shadow ${className}`}>
-      {children}
-    </button>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SeguridadApp from './App'
+import Videos from './pages/Videos'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SeguridadApp />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SeguridadApp />} />
+        <Route path="/videos" element={<Videos />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
