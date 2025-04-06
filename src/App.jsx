4ba30e191e4285f56433a61ed502267080b3c951
Pedro Cabrera <pedrocabrera@Pedros-MacBook-Pro.local>
2025-04-06 12:36:33 -0400

    Subida nueva

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+
 export default function SeguridadApp() {
-    return (
-      <div className="p-6">
-        <h1 className="text-2xl font-bold text-center text-blue-600">
-          Portal de Seguridad
-        </h1>
-        <p className="text-center mt-4">Bienvenido al portal.</p>
-      </div>
-    );
-  }
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-center text-blue-600">
+        Portal de Seguridad
+      </h1>
+      <p className="text-center mt-4">Bienvenido al portal.</p>
+    </div>
+  );
+}
